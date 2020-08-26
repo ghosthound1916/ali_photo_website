ab827979040d73512d4222aba0e587d863f84c49
smokehound <robertmoncure@gmail.com>
2020-08-26 00:51:32 -0500

    Added javascript testing page

---
+++ javascript/practice.js
@@ -0,0 +1,14 @@
+console.log('hello');
+//alert('YO!')
+
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber)
+
+
+var age = prompt('What is your age?');
+
+document.getElementById('someText').innerHTML = age;
+
